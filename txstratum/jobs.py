43c665f57671fd7ae66eed0febe90472a6dce35e
Luis Helder <luislhl@gmail.com>
2022-04-13 12:05:56 -0300

    fix: handle hathor-core errors when submitting blocks

--- txstratum/jobs.py
+++ txstratum/jobs.py
@@ -79,6 +79,8 @@ class TxJob:
         self._timeout_timer: Optional[TimerHandle] = None
         self._cleanup_timer: Optional[TimerHandle] = None
 
+        self.propagation_retry_count: int = 0
+
     def get_tx(self) -> BaseTransaction:
         """Return the Transaction object of this job."""
         return self._tx
@@ -126,6 +128,14 @@ class TxJob:
         assert self.started_at is not None
 
         return self.started_at - self.created_at
+
+    def increase_propagation_retry_count(self) -> int:
+        """Increase the number of times we tried to propagate this tx
+
+           Return the updated count
+        """
+        self.propagation_retry_count += 1
+        return self.propagation_retry_count
 
     def to_dict(self) -> Dict[str, Any]:
         """Return a dict with an overview of the job.
--- txstratum/manager.py
+++ txstratum/manager.py
@@ -40,6 +40,9 @@ class TxMiningManager:
     DEFAULT_BLOCK_TEMPLATE_UPDATE_INTERVAL = 3.0  # seconds
     TX_CLEAN_UP_INTERVAL = 300.0  # seconds
 
+    MAX_RETRY_FOR_FAILED_PUSH_TX = 3
+    RETRY_WAIT_FOR_FAILED_PUSH_TX = 3 # seconds
+
     def __init__(
         self,
         backend: "HathorClient",
@@ -153,6 +156,60 @@ class TxMiningManager:
 
         protocol.update_job(job, clean=clean)
 
+    async def push_job(self, job: MinerJob, sleep: Optional[int] = None) -> None:
+        """Pushes a block or transaction to the fullnode
+
+        :param job: The MinerJob wrapping the block or transactions
+        :param sleep: An optional sleep to force waiting some time before pushing, defaults to None
+        """
+        if sleep:
+            await asyncio.sleep(sleep)
+
+        asyncio.ensure_future(
+            self.backend.push_tx_or_block(job.get_data())
+        ).add_done_callback(
+            lambda future: self.handle_submitted_solution(future, job)
+        )
+
+
+    def handle_submitted_solution(
+        self, future: asyncio.Future, job: MinerJob
+    ) -> None:
+        """Handles a response from the fullnode for a submitted job.
+           This is expected to be used as an asyncio callback with `add_done_callback` method.
+
+        :param future: The future for which this callback was called
+        :param job: The job originally holding the submitted block or transaction
+        """
+        if not future.exception():
+            if job.is_block:
+                self.latest_submitted_block_height = job.height
+                # XXX Should we stop all other miners from mining?
+                asyncio.ensure_future(self.update_block_template())
+                self.log.info("Block found", job=job.uuid.hex())
+                self.blocks_found += 1
+            else:
+                self.log.info(
+                    "TxJob propagated", tx_job=tx_job.to_dict()
+                )
+
+            return
+
+        # Treat the error differently depending on whether it was a block or transaction
+        if job.is_block:
+            self.log.error(f"Error when submitting block: {future.exception()}", job=job)
+        else:
+            assert isinstance(job, MinerTxJob)
+            tx_job = job.tx_job
+            retry_count = tx_job.increase_propagation_retry_count()
+
+            if retry_count > self.MAX_RETRY_FOR_FAILED_PUSH_TX:
+                self.log.error(f"Error when propagating tx_job: {future.exception()}; Giving up on retrying after {retry_count - 1} failed attempts", tx_job=tx_job.to_dict())
+            else:
+                self.log.error(f"Error when propagating tx_job: {future.exception()}; Will try again in {self.RETRY_WAIT_FOR_FAILED_PUSH_TX} seconds..", tx_job=tx_job.to_dict())
+
+                asyncio.ensure_future(self.push_job(job, sleep=self.RETRY_WAIT_FOR_FAILED_PUSH_TX))
+
     def submit_solution(
         self, protocol: StratumProtocol, job: MinerJob, nonce: bytes
     ) -> None:
@@ -160,14 +217,12 @@ class TxMiningManager:
         if job.is_block:
             assert isinstance(job, MinerBlockJob)
             if job.height <= self.latest_submitted_block_height:
+                self.log.info(
+                    f"Ignoring submission of a block with height {job.height}, because we already submitted a block with height {self.latest_submitted_block_height}.",
+                    job=job,
+                )
                 return
-            self.latest_submitted_block_height = job.height
-            asyncio.ensure_future(self.backend.push_tx_or_block(job.get_data()))
-            # XXX Should we stop all other miners from mining?
-            asyncio.ensure_future(self.update_block_template())
-            self.log.info("Block found", job=job.uuid.hex())
-            self.blocks_found += 1
-
+            asyncio.ensure_future(self.push_job(job))
         else:
             assert isinstance(job, MinerTxJob)
             tx_job = job.tx_job
@@ -195,8 +250,7 @@ class TxMiningManager:
             tx_job.mark_as_solved(nonce=nonce, timestamp=job.get_object().timestamp)
             self.stop_mining_tx(tx_job)
             if tx_job.propagate:
-                tx_bytes = bytes(tx_job.get_tx())
-                asyncio.ensure_future(self.backend.push_tx_or_block(tx_bytes))
+                asyncio.ensure_future(self.push_job(job))
             self.log.info(
                 "TxJob solved", propagate=tx_job.propagate, tx_job=tx_job.to_dict()
             )
